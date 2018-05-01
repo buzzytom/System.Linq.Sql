b6d7eb88e9edab2eabb006806f102bf664bf7a27
Thomas Williams <thomas7.williams@hotmail.com>
2018-05-01 16:31:16 +0100

    Created MockExpressionVisitor.cs. Implemented tests in FieldExpressionTests.cs.

--- src/LinqSql.Tests/Expressions/Implementations/FieldExpressionTests.cs
+++ src/LinqSql.Tests/Expressions/Implementations/FieldExpressionTests.cs
@@ -1,16 +1,48 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using System;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace LinqSql.Expressions.Tests
 {
     [TestClass]
     public class FieldExpressionTests
     {
+        private ASourceExpression source = new TableExpression("Table", "Alias", new string[] { "FieldName" });
         private FieldExpression expression = null;
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            expression = new FieldExpression(source, "FieldName", "FieldAlias");
+        }
+
+        [TestMethod]
+        public void FieldExpression_Constructor_Exceptions()
+        {
+            Assert.ThrowsException<ArgumentNullException>(() => new FieldExpression(null, "FieldName", "FieldAlias"));
+            Assert.ThrowsException<ArgumentException>(() => new FieldExpression(source, "", "FieldAlias"));
+            Assert.ThrowsException<ArgumentException>(() => new FieldExpression(source, "FieldName", ""));
+        }
 
         [TestMethod]
         public void FieldExpression_Properties()
         {
-            Assert.Fail();
+            Assert.AreEqual("FieldName", expression.Field);
+            Assert.AreEqual("FieldAlias", expression.Alias);
+            Assert.AreSame(source, expression.Source);
+        }
+
+        [TestMethod]
+        public void FieldExpression_Accept()
+        {
+            // Setup test
+            MockExpressionVisitor visitor = new MockExpressionVisitor();
+
+            // Perform the test operation
+            expression.Accept(visitor);
+
+            // Check test result
+            Assert.IsTrue(visitor.FieldVisited);
         }
     }
 }
---
+++ src/LinqSql.Tests/Expressions/MockExpressionVisitor.cs
@@ -0,0 +1,28 @@
+﻿namespace LinqSql.Expressions.Tests
+{
+    public class MockExpressionVisitor : AExpressionVisitor
+    {
+        public override void VisitField(FieldExpression expression)
+        {
+            FieldVisited = true;
+        }
+
+        public override void VisitSelect(SelectExpression expression)
+        {
+            SelectVisited = true;
+        }
+
+        public override void VisitTable(TableExpression expression)
+        {
+            TableVisited = true;
+        }
+
+        // ----- Properties ----- //
+
+        public bool FieldVisited { private set; get; } = false;
+
+        public bool SelectVisited { private set; get; } = false;
+
+        public bool TableVisited { private set; get; } = false;
+    }
+}
