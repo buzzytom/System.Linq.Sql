b688ec9fd46dded560dfd97c5307af3c830d00c0
Thomas Williams <thomas7.williams@hotmail.com>
2018-05-01 17:14:37 +0100

    Added intellisense comments to FieldExpressions.cs. Implemented unit tests in FieldExpressionsTests.cs.

--- src/LinqSql.Tests/Expressions/FieldExpressionsTests.cs
+++ src/LinqSql.Tests/Expressions/FieldExpressionsTests.cs
@@ -1,16 +1,42 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using System.Linq;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace LinqSql.Expressions.Tests
 {
     [TestClass]
     public class FieldExpressionsTests
     {
-        private FieldExpressions expression = null;
+        [TestMethod]
+        public void FieldExpressions_GetEnumerator_Empty()
+        {
+            // Prepare test data
+            FieldExpressions expressions = new FieldExpressions();
+
+            // Check test result
+            Assert.AreEqual(0, expressions.Count());
+            foreach (FieldExpression field in expressions)
+                Assert.Fail();
+        }
 
         [TestMethod]
-        public void FieldExpressions_Properties()
+        public void FieldExpressions_GetEnumerator_Populated()
         {
-            Assert.Fail();
+            // Prepare test data
+            string[] fields = new string[] { "FieldA", "FieldB" };
+            ASourceExpression source = new TableExpression("Table", "Alias", fields);
+            FieldExpressions expressions = new FieldExpressions(source, fields);
+
+            // Check test result
+            Assert.AreEqual(fields.Length, expressions.Count());
+            int next = 0;
+            foreach (FieldExpression field in expressions)
+            {
+                Assert.AreSame(source, field.Source);
+                Assert.IsTrue(fields.Any(x => x == field.Field));
+                Assert.AreEqual($"f{next}", field.Alias);
+                next++;
+            }
         }
     }
 }
--- src/LinqSql/Expressions/FieldExpressions.cs
+++ src/LinqSql/Expressions/FieldExpressions.cs
@@ -10,10 +10,18 @@ namespace LinqSql.Expressions
         private int next = 0;
         private LinkedList<FieldExpression> expressions = new LinkedList<FieldExpression>();
 
+        /// <summary>
+        /// Initializes a new empty instance of <see cref="FieldExpressions"/>.
+        /// </summary>
         public FieldExpressions()
         {
         }
 
+        /// <summary>
+        /// Initializes a new instance of <see cref="FieldExpression"/> with the specified fields. The generated <see cref="FieldExpression"/> instances all share the same specified source.
+        /// </summary>
+        /// <param name="source">The source to use for each generated <see cref="FieldExpression"/>.</param>
+        /// <param name="fields">The names of the fields on the source.</param>
         public FieldExpressions(ASourceExpression source, IEnumerable<string> fields)
         {
             if (fields == null)
@@ -25,6 +33,11 @@ namespace LinqSql.Expressions
                 Add(source, field);
         }
 
+        /// <summary>
+        /// Adds a new <see cref="FieldExpression"/> to the collection using the specified source and field name.
+        /// </summary>
+        /// <param name="source">The source which the field exists on.</param>
+        /// <param name="field">The name of the field on the source.</param>
         public void Add(ASourceExpression source, string field)
         {
             FieldExpression found = source.Fields.FirstOrDefault(x => x.Alias == field);
@@ -35,14 +48,22 @@ namespace LinqSql.Expressions
             next++;
         }
 
+        /// <summary>
+        /// Returns an enumerator that iterates through the collection.
+        /// </summary>
+        /// <returns>An enumerator that can be used to iterate through the collection.</returns>
         public IEnumerator<FieldExpression> GetEnumerator()
         {
             return expressions.GetEnumerator();
         }
 
+        /// <summary>
+        /// Returns an enumerator that iterates through the collection.
+        /// </summary>
+        /// <returns>An enumerator that can be used to iterate through the collection.</returns>
         IEnumerator IEnumerable.GetEnumerator()
         {
-            return expressions.GetEnumerator();
+            return GetEnumerator();
         }
     }
 }
