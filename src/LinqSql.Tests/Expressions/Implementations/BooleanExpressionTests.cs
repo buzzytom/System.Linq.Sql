9168d25d46022976df09ef1e11fdcbfe25992205
Thomas Williams <thomas7.williams@hotmail.com>
2018-05-07 15:31:41 +0100

    Created and Implemented BooleanExpressionTests.cs.

---
+++ src/LinqSql.Tests/Expressions/Implementations/BooleanExpressionTests.cs
@@ -0,0 +1,33 @@
+﻿using System.Linq.Expressions;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace System.Linq.Sql.Expressions.Tests
+{
+    [TestClass]
+    public class BooleanExpressionTests
+    {
+        private readonly BooleanExpression expression = new BooleanExpression(true);
+
+        [TestMethod]
+        public void BooleanExpression_Properties()
+        {
+            Assert.AreEqual(ExpressionType.Extension, expression.NodeType);
+            Assert.AreEqual(typeof(bool), expression.Type);
+            Assert.IsTrue(expression.Value);
+        }
+
+        [TestMethod]
+        public void BooleanExpression_Accept()
+        {
+            // Setup test
+            MockExpressionVisitor visitor = new MockExpressionVisitor();
+
+            // Perform the test operation
+            visitor.Visit(expression);
+
+            // Check test result
+            Assert.IsTrue(visitor.BooleanVisited);
+        }
+    }
+}
