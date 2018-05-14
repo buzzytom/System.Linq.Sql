864f23baf60bce98c35edae6bb1eea67d394dcc7
Thomas Williams <thomas7.williams@hotmail.com>
2018-05-14 14:24:24 +0100

    Created QueryHelper.cs defining the methods: SelectRecordItems and Flatten.

---
+++ src/LinqSql/Queryable/QueryHelper.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+
+namespace System.Linq.Sql
+{
+    public static class QueryHelper
+    {
+        public static IEnumerable<RecordItem> SelectRecordItems(this IEnumerable<Record> records)
+        {
+            if (records == null)
+                throw new ArgumentNullException(nameof(records));
+
+            return records.Select(x => x.Values.FirstOrDefault());
+        }
+
+        public static IEnumerable<RecordItem> SelectRecordItems(this IEnumerable<Record> records, string table)
+        {
+            if (records == null)
+                throw new ArgumentNullException(nameof(records));
+            if (string.IsNullOrWhiteSpace(table))
+                throw new ArgumentException("Cannot be whitespace.", nameof(table));
+
+            return records.Select(x => x[table]);
+        }
+
+        public static IEnumerable<ILookup<string, object>> Flatten(this IEnumerable<Record> records)
+        {
+            if (records == null)
+                throw new ArgumentNullException(nameof(records));
+
+            return records.Select((Record record) =>
+            {
+                return record
+                    .SelectMany(x => x.Value)
+                    .ToLookup(x => x.Key, x => x.Value);
+            });
+        }
+    }
+}
