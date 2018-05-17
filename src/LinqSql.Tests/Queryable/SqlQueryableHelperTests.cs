a97f89639b5e31f68531ec302c9f102e3ef027f1
Thomas Williams <thomas7.williams@hotmail.com>
2018-05-17 09:02:06 +0100

    Modified the Join extension method in SqlQueryableHelper.cs to only work with Records instead of using generics. Created SqlQueryableHelperTests.cs currently only defining the SqlQueryableHelper_Join_ArgumentExceptions tests.

---
+++ src/LinqSql.Tests/Queryable/SqlQueryableHelperTests.cs
@@ -0,0 +1,31 @@
+﻿using System.Data.Common;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace System.Linq.Sql.Tests
+{
+    [TestClass]
+    public class SqlQueryableHelperTests
+    {
+        private readonly DbConnection connection = ConnectionTestHelper.CreateConnection();
+        private APredicateExpression predicate = new BooleanExpression(true);
+        private SqlQueryable outer = null;
+        private SqlQueryable inner = null;
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            outer = new SqlQueryable(connection, "OuterTable", new[] { "OuterField" });
+            inner = new SqlQueryable(connection, "InnerTable", new[] { "InnerField" });
+        }
+
+        [TestMethod]
+        public void SqlQueryableHelper_Join_ArgumentExceptions()
+        {
+            Assert.ThrowsException<ArgumentNullException>(() => SqlQueryableHelper.Join(null, inner, (i, o) => true, (i, o) => i, JoinType.Inner));
+            Assert.ThrowsException<ArgumentNullException>(() => SqlQueryableHelper.Join(outer, null, (i, o) => true, (i, o) => i, JoinType.Inner));
+            Assert.ThrowsException<ArgumentNullException>(() => SqlQueryableHelper.Join(outer, inner, null, (i, o) => i, JoinType.Inner));
+            Assert.ThrowsException<ArgumentNullException>(() => SqlQueryableHelper.Join(outer, inner, (i, o) => true, null, JoinType.Inner));
+        }
+    }
+}
--- src/LinqSql/Queryable/SqlQueryableHelper.cs
+++ src/LinqSql/Queryable/SqlQueryableHelper.cs
@@ -1,10 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq.Expressions;
+﻿using System.Linq.Expressions;
 using System.Reflection;
-using System.Text;
 
-namespace System.Linq.Sql.Queryable
+namespace System.Linq.Sql
 {
     /// <summary>
     /// Provides a set of static methods for querying <see cref="SqlQueryable"/> instances.
@@ -14,17 +11,14 @@ namespace System.Linq.Sql.Queryable
         /// <summary>
         /// Correlates the elements of two sequences based on the specifed predicate.
         /// </summary>
-        /// <typeparam name="TOuter">The type of the elements of the first sequence.</typeparam>
-        /// <typeparam name="TInner">The type of the elements of the second sequence.</typeparam>
-        /// <typeparam name="TResult"></typeparam>
         /// <param name="outer">The first sequence to join.</param>
         /// <param name="inner">The sequence to join to the first sequence.</param>
         /// <param name="predicate">A function to test each element of each sequence for a condition.</param>
         /// <param name="resultSelector">A function to create a result element from two matching elements.</param>
         /// <param name="comparer">The type of join this call represents.</param>
-        /// <returns>An <see cref="IQueryable{T}"/> that has elements of type <see cref="{TResult}"/> obtained by performing a join on the two sequences.</returns>
+        /// <returns>An <see cref="IQueryable{Record}"/> that has elements obtained by performing a join on the two sequences.</returns>
         /// <exception cref="ArgumentNullException">outer, inner, predicate or resultSelector is null.</exception>
-        public static IQueryable<TResult> Join<TOuter, TInner,  TResult>(this IQueryable<TOuter> outer, IQueryable<TInner> inner, Expression<Func<TOuter, TInner, bool>> predicate, Expression<Func<TOuter, TInner, Record>> resultSelector, JoinType joinType = JoinType.Inner)
+        public static IQueryable<Record> Join(this IQueryable<Record> outer, IQueryable<Record> inner, Expression<Func<Record, Record, bool>> predicate, Expression<Func<Record, Record, Record>> resultSelector, JoinType joinType = JoinType.Inner)
         {
             if (outer == null)
                 throw new ArgumentNullException(nameof(outer));
@@ -35,19 +29,18 @@ namespace System.Linq.Sql.Queryable
             if (resultSelector == null)
                 throw new ArgumentNullException(nameof(resultSelector));
 
-            return outer.Provider.CreateQuery<TResult>(
+            return outer.Provider.CreateQuery<Record>(
                 Expression.Call(
                     null,
-                    MethodBase.GetCurrentMethod(),
+                    (MethodInfo)MethodBase.GetCurrentMethod(),
                     new Expression[]
                     {
                         outer.Expression,
                         inner.Expression,
                         Expression.Quote(predicate),
                         Expression.Quote(resultSelector),
-                        Expression.Constant(joinType);
+                        Expression.Constant(joinType)
+                    }));
         }
-                    ));
-        }
-}
+    }
 }
