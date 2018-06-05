5eb03d6109571b9d43b73cba606e27682a6b5374
Thomas Williams <thomas7.williams@hotmail.com>
2018-06-05 14:25:41 +0100

    Added an Orderings property to SelectExpression.cs using a new Ordering class and OrderType enum.

--- src/LinqSql/Expressions/Implementations/SelectExpression.cs
+++ src/LinqSql/Expressions/Implementations/SelectExpression.cs
@@ -4,6 +4,42 @@ using System.Linq.Expressions;
 namespace System.Linq.Sql
 {
     /// <summary>
+    /// An enumeration of the order directions supported by sql.
+    /// </summary>
+    public enum OrderType
+    {
+        /// <summary>Values will be sorted such that the next item is greater than or equal to its previous value.</summary>
+        Ascending,
+        /// <summary>Values will be sorted such that the next item is less than or equal to its previous value.</summary>
+        Descending
+    }
+
+    /// <summary>
+    /// <see cref="Ordering"/> represents the ordering direction of a single column.
+    /// </summary>
+    public class Ordering
+    {
+        /// <summary>
+        /// Initializes a new instance of <see cref="Ordering"/> with the specified field and order direction.
+        /// </summary>
+        /// <param name="field">The field to order.</param>
+        /// <param name="type">The direction to order the column.</param>
+        public Ordering(FieldExpression field, OrderType type)
+        {
+            Field = field;
+            OrderType = type;
+        }
+
+        // ----- Properties ----- //
+
+        /// <summary>Gets the Field of this object instance.</summary>
+        public FieldExpression Field { get; }
+
+        /// <summary>Gets the OrderType of this object instance.</summary>
+        public OrderType OrderType { get; }
+    }
+
+    /// <summary>
     /// Represents the field selection on a source expression.
     /// </summary>
     public class SelectExpression : ASourceExpression
@@ -12,10 +48,11 @@ namespace System.Linq.Sql
         /// Initializes a new instance of <see cref="SelectExpression"/> selecting all the fields from the specified source.
         /// </summary>
         /// <param name="source">The source expression to select from.</param>
-        /// /// <param name="take">The number of fields to take from the source. Values less than zero indicate all rows.</param>
+        /// <param name="take">The number of fields to take from the source. Values less than zero indicate all rows.</param>
         /// <param name="skip">The number of fields to ignore on the source before reading rows.</param>
-        public SelectExpression(ASourceExpression source, int take = -1, int skip = 0)
-            : this(source, source?.Fields, take, skip)
+        /// <param name="orderings">The optional sorting of the selected rows.</param>
+        public SelectExpression(ASourceExpression source, int take = -1, int skip = 0, IEnumerable<Ordering> orderings = null)
+            : this(source, source?.Fields, take, skip, orderings)
         { }
 
         /// <summary>
@@ -25,7 +62,8 @@ namespace System.Linq.Sql
         /// <param name="fields">The fields to select from the source.</param>
         /// <param name="take">The number of fields to take from the source. Values less than zero indicate all rows.</param>
         /// <param name="skip">The number of fields to ignore on the source before reading rows.</param>
-        public SelectExpression(ASourceExpression source, IEnumerable<FieldExpression> fields, int take = -1, int skip = 0)
+        /// <param name="orderings">The optional sorting of the selected rows.</param>
+        public SelectExpression(ASourceExpression source, IEnumerable<FieldExpression> fields, int take = -1, int skip = 0, IEnumerable<Ordering> orderings = null)
         {
             if (fields == null)
                 throw new ArgumentNullException(nameof(fields));
@@ -37,6 +75,11 @@ namespace System.Linq.Sql
             Expressions = new[] { source };
             Take = take;
             Skip = skip;
+
+            if (orderings != null)
+            {
+                // TODO - Copy the orderings, getting their field from the new Fields.
+            }
         }
 
         /// <summary>
@@ -65,5 +108,8 @@ namespace System.Linq.Sql
 
         /// <summary>Gets the number of rows that will be taken from the source expression.</summary>
         public int Take { get; } = -1;
+
+        /// <summary>Gets the orderings that will be applied to the source expression.</summary>
+        public IEnumerable<Ordering> Orderings { get; } = null;
     }
 }
